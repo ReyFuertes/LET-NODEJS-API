dba876f37fdb4b64f0fecf46c60a5ce4d2e1ab28
rfuertes <rfuertes@cyrious.com>
2017-12-09 23:43:12 +0800

    init repo

---
+++ v1/config/config.js
@@ -0,0 +1,9 @@
+var config = {};
+
+config.email = 'let.emailer@gmail.com';
+config.email_pass = 'p@55w0rd';
+config.email_subject = 'LET Reviewer System - Verification'
+config.server_address = 'http://localhost:4200'
+config.reset_password_subjet = 'Request Reset Password';
+
+module.exports = config;
