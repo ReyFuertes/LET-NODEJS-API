e3a2d3fcf0ad3eafd9f1e32b1e56a536a6a1ce59
rfuertes <rfuertes@cyrious.com>
2017-12-17 07:24:29 +0800

    rm pass

--- v1/config/config.js
+++ v1/config/config.js
@@ -1,7 +1,7 @@
 var config = {};
 
 config.email = 'let.emailer@gmail.com';
-config.email_pass = 'p@55w0rd';
+config.email_pass = '';
 config.email_subject = 'LET Reviewer System - Verification'
 config.server_address = 'http://localhost:4200'
 config.reset_password_subjet = 'Request Reset Password';
